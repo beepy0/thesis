fc580c36e07161d38c9401600ff2365fc86648aa
Branimir-Ubuntu-Home <bpavlov@dentolo.de>
2019-06-09 22:27:31 +0200

    Removed more unneeded files.

--- optimization/cmake-build-debug/CMakeFiles/optimization.dir/cmake_clean.cmake
+++ optimization/cmake-build-debug/CMakeFiles/optimization.dir/cmake_clean.cmake
@@ -2,7 +2,6 @@ file(REMOVE_RECURSE
   "CMakeFiles/optimization.dir/main.cpp.o"
   "CMakeFiles/optimization.dir/sketches.cpp.o"
   "CMakeFiles/optimization.dir/xis.cpp.o"
-  "CMakeFiles/optimization.dir/relation.cpp.o"
   "optimization.pdb"
   "optimization"
 )
--- optimization/main.cpp
+++ optimization/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <algorithm> // for sorting
-#include "relation.h"
 #include "xis.h"
 
 #include "sketches.h"
