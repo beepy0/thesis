9f38a8971556d01275684a4255f7896608faefa0
Branimir-Ubuntu-Home <bpavlov@dentolo.de>
2019-06-27 21:30:57 +0200

    Commented out lines, moved helper functions to separate file.

--- optimization/main.cpp
+++ optimization/main.cpp
@@ -11,172 +11,43 @@
 #include <string>
 #include <chrono>  // for high_resolution_clock
 #include <cmath>
-#include <simdpp/simd.h>
+#include <thread>
+//#include <simdpp/simd.h>
+#include "utils/helpers.h"
 
 
 using namespace std;
 
 
-unsigned int Random_Generate(unsigned int seed)
-{
-  srand(seed);
-  auto x = (unsigned int)rand();
-  auto h = (unsigned int)rand();
-
-  return x ^ ((h & 1u) << 31u);
-}
-
-void loadData(unsigned int dataArray[]) {
-  int i = 0;
-
-  std::ifstream fileStream ("data/zipf_stream_data_100k.csv");
-
-  if(fileStream.is_open())
-  {
-    std::string line;
-    while(std::getline(fileStream, line, '\n'))
-    {
-      dataArray[i] = (unsigned int)std::stoul(line);
-      i++;
-    }
-    fileStream.close();
-  }
-}
-
-void timeSketchUpdate(Sketch *agms1, unsigned int data[],
-                      const int tuples_no, const string &sketch_type)
-{
-  cout << endl << "updating " << sketch_type <<
-      " sketch with stream data..." << endl;
-  //update the sketches for relation
-  auto start_agms = std::chrono::high_resolution_clock::now();
-  for (int i = 0; i < tuples_no; i++)
-  {
-    agms1->Update_Sketch(data[i], 1.0);
-  }
-  auto finish_agms = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> elapsed_agms = finish_agms - start_agms;
-  double final_time_agms = elapsed_agms.count();
-  printf("done. %s with %u size: %f \n\n", sketch_type.c_str(),
-                                           tuples_no,
-                                           final_time_agms);
-}
-
-void computeManualFrequencyVector(const unsigned int data[],
-                                  unsigned int freq_vector[],
-                                  const int tuples_no)
-{
-  unsigned int current_value = 1;
-  int freq_vector_index = 0;
-  for(int i = 0; i < tuples_no; i++)
-  {
-    if (data[i] == 0)
-    {
-      continue;
-    }
-    if(data[i] == current_value)
-    {
-      freq_vector[freq_vector_index] += 1;
-    } else
-      {
-        freq_vector_index++;
-        current_value = data[i];
-        freq_vector[freq_vector_index] += 1;
-      }
-  }
-}
-
-void printFrequencies(const unsigned int freq_vector[], const int tuples_no)
-{
-  cout << endl << "Real frequencies: " << endl;
-  for(int i = 0; i < tuples_no; i++)
-  {
-    if(freq_vector[i] != 0)
-    {
-      cout << freq_vector[i] << ' ';
-    }
-  }
-  cout << endl << endl;
-}
-
-long long computeManualSelfJoinSize(const unsigned int freq_vector[],
-                                    const int tuples_no)
-{
-  long long tmp = 0;
-  long long self_join_size = 0;
-  for(int i = 0; i < tuples_no; i++)
-  {
-    tmp = freq_vector[i];
-    self_join_size += (tmp * tmp);
-  }
-  return self_join_size;
-}
-
-
-unsigned int computeTruncationMask(const unsigned int buckets_no)
-{
-  auto usable_bits_size = (unsigned int)ceil(log2(buckets_no));
-  unsigned int result = 0u;
-  for(unsigned int i = 0; i < usable_bits_size; i++)
-  {
-    result += (unsigned int)pow(2, i);
-  }
-
-  return result;
-}
-
-
-//void get_key_bits(unsigned int key, unsigned *bits)
-//{
-//  unsigned
-//      n_bits = 32u,
-//      bit = 0;
-//
-//  for(bit = 0; bit < n_bits; ++bit)
-//    bits[n_bits - 1 - bit] = (key >> bit) & 1;
-//
-//}
-
-
 int main() {
 
-//  const unsigned int key = 0xfffffff0;
-//  unsigned int key = 0b011101u;
-//  unsigned int no_bits = 32u;
-//  auto *key_bits = (unsigned*)malloc(sizeof(unsigned) * no_bits);
-//  get_key_bits(key, key_bits);
-//
-//
-//  cout << endl;
-//  for(unsigned int bit = 0; bit < no_bits; bit++)
-//    printf("%u", key_bits[bit]);
-//  printf("\n");
-
-
-
   unsigned int i;
-  const unsigned int buckets_no = 128;
+  const unsigned int buckets_no = 129;
   const unsigned int truncation_mask = computeTruncationMask(buckets_no);
   auto floor_offset = (unsigned int)floor(log2(buckets_no));
   auto floor_value = (unsigned int)pow(2, floor_offset);
-  const unsigned int rows_no = 130;
-  const int tuples_no = 100000;
+  const unsigned int rows_no = 129;
+  const int tuples_no = 1000000;
   unsigned int data[tuples_no] = {0};
 
   loadData(data);
   sort(data, data + tuples_no);
 
-  unsigned int freq_vector[tuples_no] = {0};
-  computeManualFrequencyVector(data, freq_vector, tuples_no);
-  printFrequencies(freq_vector, tuples_no);
-  long long manual_join_size =
-      computeManualSelfJoinSize(freq_vector, tuples_no);
-  cout << "Real join size computation is: " << manual_join_size << endl;
+//  unsigned int freq_vector[tuples_no] = {0};
+//  computeManualFrequencyVector(data, freq_vector, tuples_no);
+//  printFrequencies(freq_vector, tuples_no);
+//  long long manual_join_size =
+//      computeManualSelfJoinSize(freq_vector, tuples_no);
+//  cout << "Real join size computation is: " << manual_join_size << endl;
 
   unsigned int I1, I2;
   //fix random seed variables for cross-platform for testing
-  I1 = 2737495323u;
-  I2 = 3193102325u;
+//  I1 = 2737495323u;
+//  I2 = 3193102325u;
+
+  I1 = Random_Generate((unsigned int)rand());
+  I2 = Random_Generate((unsigned int)rand());
+
   //generate the pseudo-random numbers for AGMS sketches; use EH3
   auto **agms_eh3 = new Xi*[buckets_no * rows_no];
   for (i = 0; i < buckets_no * rows_no; i++)
@@ -190,7 +61,7 @@ int main() {
   for (i = 0; i < rows_no; i++)
   {
     fagms_eh3[i] = new Xi_EH3(I1, I2);
-    fagms_h3[i] = new Xi_H3B(1333337u, 32u, truncation_mask,
+    fagms_h3[i] = new Xi_H3B((unsigned int)rand(), 32u, truncation_mask,
                                 floor_offset, floor_value);
   }
 
@@ -199,8 +70,9 @@ int main() {
   Sketch *fagms1 = new FAGMS_Sketch(buckets_no, rows_no,
                                     fagms_h3, fagms_eh3);
 
-  timeSketchUpdate(agms1, data, tuples_no, "AGMS");
-  timeSketchUpdate(fagms1, data, tuples_no, "Fast-AGMS");
+//  timeSketchUpdate(agms1, data, tuples_no, "AGMS");
+  timeSketchUpdate(fagms1, data, tuples_no, "Fast-AGM`S");
+
   // multiple run avg time testing
 //  int runs = 1;
 //  double sum = 0.0;
@@ -213,8 +85,8 @@ int main() {
 
 
   //compute the sketch estimate
-  double agms_est = agms1->Self_Join_Size();
-  double fagms_est = fagms1->Self_Join_Size();
+//  double agms_est = agms1->Self_Join_Size();
+//  double fagms_est = fagms1->Self_Join_Size();
 
   //clean-up everything
   for (i = 0; i < buckets_no * rows_no; i++)
@@ -232,8 +104,8 @@ int main() {
   delete agms1;
   delete fagms1;
 
-  printf("\n AGMS Estimate is: %20.2f \n\n", agms_est);
-  printf("\n Fast-AGMS Estimate is: %20.2f \n\n", fagms_est);
+//  printf("\n AGMS Estimate is: %20.2f \n\n", agms_est);
+//  printf("\n Fast-AGMS Estimate is: %20.2f \n\n", fagms_est);
 
   return 0;
 }
---
+++ optimization/utils/helpers.h
@@ -0,0 +1,128 @@
+//
+// Created by meggamorty on 6/25/19.
+//
+
+#ifndef OPTIMIZATION_HELPERS_H
+#define OPTIMIZATION_HELPERS_H
+
+unsigned int Random_Generate(unsigned int seed)
+{
+  srand(seed);
+  auto x = (unsigned int)rand();
+  auto h = (unsigned int)rand();
+
+  return x ^ ((h & 1u) << 31u);
+}
+
+void loadData(unsigned int dataArray[]) {
+  int i = 0;
+
+  std::ifstream fileStream ("data/uniform_stream_data_1000k.csv");
+
+  if(fileStream.is_open())
+  {
+    std::string line;
+    while(std::getline(fileStream, line, '\n'))
+    {
+      dataArray[i] = (unsigned int)std::stoul(line);
+      i++;
+    }
+    fileStream.close();
+  }
+}
+
+void timeSketchUpdate(Sketch *agms1, unsigned int data[],
+                      const int tuples_no, const string &sketch_type)
+{
+//  cout << endl << "updating " << sketch_type <<
+//       " sketch with stream data..." << endl;
+//  //update the sketches for relation
+//  auto start_agms = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < tuples_no; i++)
+  {
+    agms1->Update_Sketch(data[i], 1.0);
+  }
+//  auto finish_agms = std::chrono::high_resolution_clock::now();
+//  std::chrono::duration<double> elapsed_agms = finish_agms - start_agms;
+//  double final_time_agms = elapsed_agms.count();
+//  printf("done. %s with %u size: %f \n\n", sketch_type.c_str(),
+//         tuples_no,
+//         final_time_agms);
+}
+
+void computeManualFrequencyVector(const unsigned int data[],
+                                  unsigned int freq_vector[],
+                                  const int tuples_no)
+{
+  unsigned int current_value = 1;
+  int freq_vector_index = 0;
+  for(int i = 0; i < tuples_no; i++)
+  {
+    if (data[i] == 0)
+    {
+      continue;
+    }
+    if(data[i] == current_value)
+    {
+      freq_vector[freq_vector_index] += 1;
+    } else
+    {
+      freq_vector_index++;
+      current_value = data[i];
+      freq_vector[freq_vector_index] += 1;
+    }
+  }
+}
+
+void printFrequencies(const unsigned int freq_vector[], const int tuples_no)
+{
+  cout << endl << "Real frequencies: " << endl;
+  for(int i = 0; i < tuples_no; i++)
+  {
+    if(freq_vector[i] != 0)
+    {
+      cout << freq_vector[i] << ' ';
+    }
+  }
+  cout << endl << endl;
+}
+
+long long computeManualSelfJoinSize(const unsigned int freq_vector[],
+                                    const int tuples_no)
+{
+  long long tmp = 0;
+  long long self_join_size = 0;
+  for(int i = 0; i < tuples_no; i++)
+  {
+    tmp = freq_vector[i];
+    self_join_size += (tmp * tmp);
+  }
+  return self_join_size;
+}
+
+
+unsigned int computeTruncationMask(const unsigned int buckets_no)
+{
+  auto usable_bits_size = (unsigned int)ceil(log2(buckets_no));
+  unsigned int result = 0u;
+  for(unsigned int i = 0; i < usable_bits_size; i++)
+  {
+    result += (unsigned int)pow(2, i);
+  }
+
+  return result;
+}
+
+
+//void get_key_bits(unsigned int key, unsigned *bits)
+//{
+//  unsigned
+//      n_bits = 32u,
+//      bit = 0;
+//
+//  for(bit = 0; bit < n_bits; ++bit)
+//    bits[n_bits - 1 - bit] = (key >> bit) & 1;
+//
+//}
+
+#endif //OPTIMIZATION_HELPERS_H
